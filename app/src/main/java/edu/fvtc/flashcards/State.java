38c67446325edec03dfcbb3630eb7575a789dd3c
Cesar Hinojosa <300077836@fvtc.edu>
2024-02-16 13:24:55 -0600

    Initial Creation

---
+++ app/src/main/java/edu/fvtc/flashcards/State.java
@@ -0,0 +1,28 @@
+package edu.fvtc.flashcards;
+
+public class State {
+    public State(String name, String capital) {
+        this.name = name;
+        this.capital = capital;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    private String name;
+    private String capital;
+
+}
